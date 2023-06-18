b43b6e7b3a7c53db50a954e353a2830219e7ef45
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-18 18:56:44 +0300

    completed payee rate calculator

--- challenge3.js
+++ challenge3.js
@@ -3,13 +3,16 @@ function netSalaryCalculator(basicSalary,benefits){
    let grossIncome= basicSalary+benefits;
    return grossIncome;
 }
-function payeeMonthlyRate(payee) {
-    payee=grossIncome;
-    if (payee<=24000){
-        payee=payee*0.10;
-    }else if (payee>2400&&payee<=32333){
-        payee=payee*0.25;
-    }
 
+function payeeMonthlyRate(grossIncome) {
+    if (grossIncome<=24000){
+        payee=grossIncome*0.10;
+    }else if (grossIncome>24000&&grossIncome<=32333){
+        payee=grossIncome*0.25;
+    }else{
+        payee=grossIncome*0.30
+    }
+return payee
 }
+console.log(payeeMonthlyRate(23000));
 
