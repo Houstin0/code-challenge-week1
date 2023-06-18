83c624b60df9b7e952d29b63f07d3d24d1bc6bc1
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-18 19:07:24 +0300

    payee below 24000 value added to 0

--- challenge3.js
+++ challenge3.js
@@ -5,14 +5,27 @@ function netSalaryCalculator(basicSalary,benefits){
 }
 
 function payeeMonthlyRate(grossIncome) {
-    if (grossIncome<=24000){
+    let payee;
+    if (grossIncome<24000){
+        payee=0;
+    }
+    else if (grossIncome>=24000){
         payee=grossIncome*0.10;
     }else if (grossIncome>24000&&grossIncome<=32333){
         payee=grossIncome*0.25;
+    }else if (grossIncome>32333){
+        payee=grossIncome*0.30;
     }else{
-        payee=grossIncome*0.30
+        payee
     }
-return payee
+return payee;
 }
-console.log(payeeMonthlyRate(23000));
+console.log(payeeMonthlyRate(2300));
 
+function nhifDeductionsRate(grossIncome){
+    if (grossIncome<=5999) {
+        nhifDeductions=grossIncome-150;
+    } else if (grossIncome=6000&&grossIncome<=7999) {
+        
+    }
+}
