f03fd2d21762a46020451688e7f7616584196856
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-18 19:18:09 +0300

    added nhif deductions rate

--- challenge3.js
+++ challenge3.js
@@ -23,9 +23,18 @@ return payee;
 console.log(payeeMonthlyRate(2300));
 
 function nhifDeductionsRate(grossIncome){
+    let nhifDeductions;
     if (grossIncome<=5999) {
         nhifDeductions=grossIncome-150;
-    } else if (grossIncome=6000&&grossIncome<=7999) {
-        
+    } else if (grossIncome<=7999) {
+        nhifDeductions=grossIncome-300;
+    }else if (grossIncome<=11999){
+        nhifDeductions=grossIncome-400;
+    }else if (grossIncome<=14999){
+        nhifDeductions=grossIncome-500;
+    }else if (grossIncome<=19999){
+        nhifDeductions=grossIncome-600;
+    }else if (grossIncome<=24999){
+        nhifDeductions=grossIncome-750;
     }
 }
