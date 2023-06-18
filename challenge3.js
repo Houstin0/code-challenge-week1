3dd61f666a919c6538baa602767b42fa8ce63f51
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-18 18:11:53 +0300

    challenge3

---
+++ challenge3.js
@@ -0,0 +1,13 @@
+
+function netSalaryCalculator(basicSalary,benefits){
+   let grossIncome= basicSalary+benefits
+}
+function payee() {
+    
+}
+function nhifDeductions(){
+
+}
+function nssfDeductions(){
+
+}
